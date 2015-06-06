20c89c7c3ce57729ae52da259741bf37b9373ee2
kylemorton5770 <mikemorton91@gmail.com>
2015-06-06 18:43:40 -0400

    after master code revision

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://node:noder@novus.modulusmongo.net:27017/Iganiq8o',
+	'database': 'kyle:oagland@ds041992.mongolab.com:41992/meanmongo',
 	'secret': 'ilovescotchscotchyscotchscotch'
 };
